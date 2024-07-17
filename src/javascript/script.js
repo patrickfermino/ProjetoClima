46f0bb4c5b7a8a2ee6755fb480682b759df08ca1
Patrick Fermino <136861106+patrickfermino@users.noreply.github.com>
2024-07-16 23:44:41 -0300

    Finalizando

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -7,18 +7,39 @@ document.querySelector('#search').addEventListener('submit', async (event)=>{
     if (!cityName){
         return showAlert("Você precisa digitar uma cidade");
     }       
-        const apiKey = 'd3ea3e474f4d84b06bdf2ecfb0284e84'
-        const apiUrl =  'api.openweathermap.org/data/2.5/forecast?q=${encodeURI(cityName)}&appid=${apiKey}r$units=metric&Lang=pt_br'
+   
+        const apiKey = 'ef2177cfa077b801391d46ad9ef0b17e';
+        const apiUrl =  `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(cityName)}&appid={apiKey}$units=metric&Lang=pt_br`;
         const results = await fetch(apiUrl);
         const json = await results.json();
 
         if(json.cod === 200){
+            showInfo({
+                city: json.name,
+                country: json.sys.country,
+                temp: json.main.temp,
+                temMax: json.main.temp_max,
+                temMain: json.main.temp_min
+             /*  description: json.weather[0].description,
+               tempIcon: json.weather[0].icon,
+               windSpeed: json.wind.speed,
+               humidity: json.main.humidity,*/
+            });
             
         }else {
             showAlert('Não foi possível localicar...')
         }
     
 });
+
+function showInfo(json){
+    showAlert('');
+
+    document.querySelector('#weather').classList.add('show');
+
+    document.querySelector('#title').innerHTML = `${json.city}`, `${json.country}`; 
+
+}
 
 function showAlert(msg){
     document.querySelector('#alert').innerHTML = msg;
